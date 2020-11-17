7b170e9c6c5a714fe76e5f20a4f30b08a79143d3
cxf <1165024126@qq.com>
2020-11-17 20:01:23 +0800

    课堂练习-安装，selenium_1

---
+++ classwork/webAutoTest/seleniumTest/test_selenium.py
@@ -0,0 +1,18 @@
+'''
+
+1.安装
+官网：https://www.selenium.dev/
+前提 python  、 pip环境
+安装插件：pip install selenium    或者pycharm 插件安装配置
+下载drive：驱动，谷歌或者火狐等浏览器
+解压后，配置环境变量
+重启pycharm 以及cmd，执行命令chromedriver 如下则安装成功
+
+'''
+
+import selenium
+from selenium import webdriver
+
+def test_selenium():
+    driver = webdriver.Chrome()
+    driver.get("https://www.baidu.com/")
